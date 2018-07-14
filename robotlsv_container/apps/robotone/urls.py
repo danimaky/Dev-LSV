51fa22f2fa60f8dd99fbe16366916733c02b928f
Daniel Nunez <danielsistem.ing96@gmail.com>
2018-07-13 19:34:14 -0500

    Import bug fixed

--- robotlsv_container/apps/robotone/urls.py
+++ robotlsv_container/apps/robotone/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
 from rest_framework.routers import SimpleRouter
-from .views import RobotModelViewSet, Robot_view, testjesus
+from .views import RobotModelViewSet, Robot_view
 
 router = SimpleRouter()
 
